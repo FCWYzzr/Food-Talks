cac77ff63c5501f4521cd2fbb34b8c81e2805fd1
FCWYzzr <fcwyzzr@163.com>
2024-08-09 10:32:11 +0800

    Init

--- src/main/java/indi/fcwyzzr/minecraft/food_talks/common/mixin/accessors/LivingEntityAccessor.java
+++ src/main/java/indi/fcwyzzr/minecraft/food_talks/common/mixin/accessors/LivingEntityAccessor.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LivingEntity.class)
-interface LivingEntityAccessor{
+public interface LivingEntityAccessor{
     @Invoker(value = "onEffectRemoved", remap = false)
     void onEffectRemoved(MobEffectInstance pEffectInstance);
     
