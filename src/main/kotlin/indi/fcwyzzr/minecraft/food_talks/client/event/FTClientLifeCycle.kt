9eeb5dc1d3873993a6e942560a513e834c0d78e6
FCWYzzr <fcwyzzr@163.com>
2024-08-12 16:47:58 +0800

    client rendering

--- src/main/kotlin/indi/fcwyzzr/minecraft/food_talks/client/event/FTClientLifeCycle.kt
+++ src/main/kotlin/indi/fcwyzzr/minecraft/food_talks/client/event/FTClientLifeCycle.kt
@@ -1,12 +1,11 @@
 package indi.fcwyzzr.minecraft.food_talks.client.event
 
 import indi.fcwyzzr.minecraft.food_talks.FoodTalks
-import indi.fcwyzzr.minecraft.food_talks.client.renderer.PlateRenderer
+import indi.fcwyzzr.minecraft.food_talks.client.renderer.block.PlateRenderer
 import indi.fcwyzzr.minecraft.food_talks.common.block.entity.PlateBlockEntity
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
-import net.neoforged.fml.common.Mod
 import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers
 
 
