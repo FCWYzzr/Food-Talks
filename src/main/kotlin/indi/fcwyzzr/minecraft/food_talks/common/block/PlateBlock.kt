decc1604c163a0e4ec28cecf24a94d2076a819c2
FCWY <fcwyzzr@163.com>
2024-08-25 10:42:45 +0800

    fix issue #10:重构方块掉落逻辑，使其符合原版

--- src/main/kotlin/indi/fcwyzzr/minecraft/food_talks/common/block/PlateBlock.kt
+++ src/main/kotlin/indi/fcwyzzr/minecraft/food_talks/common/block/PlateBlock.kt
@@ -2,6 +2,7 @@ package indi.fcwyzzr.minecraft.food_talks.common.block
 
 import indi.fcwyzzr.minecraft.f_lib.registry.FRegistry
 import indi.fcwyzzr.minecraft.food_talks.common.block.entity.PlateBlockEntity
+import indi.fcwyzzr.minecraft.food_talks.common.item.Plate
 import indi.fcwyzzr.minecraft.food_talks.common.item.Sandwich
 import indi.fcwyzzr.minecraft.food_talks.toRegistryName
 import indi.fcwyzzr.minecraft.food_talks.toResourceLocation
@@ -12,6 +13,7 @@ import net.minecraft.core.Registry
 import net.minecraft.core.component.DataComponentMap
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceKey
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.sounds.SoundSource
 import net.minecraft.world.InteractionHand
@@ -22,15 +24,19 @@ import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.*
+import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.LevelAccessor
+import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.material.FluidState
 import net.minecraft.world.level.material.PushReaction
+import net.minecraft.world.level.storage.loot.LootParams
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
@@ -93,10 +99,12 @@ class PlateBlock private constructor(): Block(Properties.of().apply {
     ): InteractionResult {
         val entity = level.getBlockEntity(pos) as PlateBlockEntity
         if (player.isShiftKeyDown){
-            if (!player.hasInfiniteMaterials() && level.gameRules.getBoolean(GameRules.RULE_DOBLOCKDROPS))
-                doIngredientsDrop(level, pos, false)
-            else
+            if (player.hasInfiniteMaterials())
                 entity.clear()
+            else if (level is ServerLevel)
+                getDrops(state, level, pos, level.getBlockEntity(pos)).forEach{
+                    popResource(level, pos, it)
+                }
 
 
             return InteractionResult.SUCCESS
@@ -152,55 +160,15 @@ class PlateBlock private constructor(): Block(Properties.of().apply {
         }
     }
 
-    override fun onDestroyedByPlayer(
-        state: BlockState,
-        level: Level,
-        pos: BlockPos,
-        player: Player,
-        willHarvest: Boolean,
-        fluid: FluidState
-    ): Boolean {
-        if (level.gameRules.getBoolean(GameRules.RULE_DOBLOCKDROPS))
-            doIngredientsDrop(
-                level,
-                pos,
-                !player.hasInfiniteMaterials()
-            )
-        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid)
-    }
+    override fun getDrops(state: BlockState, params: LootParams.Builder) = buildList {
+        val breaker: Entity? = params.getOptionalParameter(LootContextParams.THIS_ENTITY)
+        if (breaker !is Player || !breaker.hasInfiniteMaterials())
+            add(Plate.defaultInstance)
 
-    override fun onDestroyedByPushReaction(
-        state: BlockState,
-        level: Level,
-        pos: BlockPos,
-        pushDirection: Direction,
-        fluid: FluidState
-    ) {
-        if (level.gameRules.getBoolean(GameRules.RULE_DOBLOCKDROPS))
-            doIngredientsDrop(
-                level,
-                pos,
-                true
-            )
-        super.onDestroyedByPushReaction(state, level, pos, pushDirection, fluid)
-    }
-
-    private fun doIngredientsDrop(
-        level: Level,
-        pos: BlockPos,
-        dropSelf: Boolean
-    ){
-        val entity = level.getBlockEntity(pos) as PlateBlockEntity
-
-        if (dropSelf)
-            popResource(level, pos, ItemStack(instance))
-
-
+        val entity = params.getParameter(LootContextParams.BLOCK_ENTITY) as PlateBlockEntity
         entity
             .popDisplay()
-            ?.apply{
-                popResource(level, pos, this)
-            }
+            ?.apply(::add)
             ?: run {
                 entity
                     .readOnlyIngredient
@@ -221,11 +189,7 @@ class PlateBlock private constructor(): Block(Properties.of().apply {
                         map
                     }
                     .flatMap { it.value.values }
-                    .forEach{
-                        popResource(level, pos, it)
-                    }
-
-                entity.clear()
+                    .forEach(::add)
             }
     }
 
